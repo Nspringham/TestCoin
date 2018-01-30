562afefa16f65b1f453b5bd774b4c9f9f28ee6ca
Nick Springham <Nspringham@aol.co.uk>
2018-01-30 21:13:57 +0000

    Added genesis block

--- main.js
+++ main.js
@@ -6,10 +6,31 @@ class Block{
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        //this will return a string of a hash of the combined index, prevHash, timestamp and data
+    }
+}
+
+class Blockchain{
+    constructor(){
+        //this is responsible for initializing the blockchain
+        this.chain = [this.createGenesisBlock()];
+        //this property chain will be an array of blocks with the manually made genesisBlock
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "30/01/2018", "Genesis Block", "0")
+    }
+
+    getLatestBlock(){
+        
+    }
+
+    addBlock(){
+
     }
 }
